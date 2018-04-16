952b94df62008b64dce1aaf162bdcb61583d08ad
Mike Dettmer <mike@mikedettmer.com>
2018-04-16 16:40:51 -0400

    removes bad code from ./tasks/index.js -- oops!

--- tasks/index.js
+++ tasks/index.js
@@ -1,17 +1,6 @@
 const tasks = [require('./maven'), require('./nukemaven')];
 
 module.exports = (window) => {
-    let max = {
-        name: 'Maximize',
-        run:(win)=>{
-            if (win.isMinimized()) {
-                win.restore();
-            }
-            win.maximize();
-        }
-    };
-    tasks.push(max);
-
     return {
         tasks,
         submenu: tasks.map((task) => {
